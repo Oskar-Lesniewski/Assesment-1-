7567cc44fe7323828f4828d5104e5f99dde5fc44
Oskar <Oskarles25@gmail.com>
2023-03-02 20:13:51 +0000

    99% complete, funtional, just needs comments

--- CMP1903M A01 2223/Pack.cs
+++ CMP1903M A01 2223/Pack.cs
@@ -37,10 +37,7 @@ namespace CMP1903M_A01_2223
 
                 }
                 pack.pack = shuffledPack;
-                foreach (Card card in pack.pack)
-                {
-                    Console.WriteLine(card.Display());
-                }
+                Console.WriteLine("Pack was shuffled.");
                 deal(shuffledPack);
                 return true;
             }
@@ -64,11 +61,7 @@ namespace CMP1903M_A01_2223
                 pack.pack = shuffledPack;
                 shuffledPack.Reverse();
 
-                Console.WriteLine("Pack was shuffled:");
-                foreach (Card card in pack.pack)
-                {
-                    Console.WriteLine(card.Display());
-                }
+                Console.WriteLine("Pack was shuffled.");
                 deal(shuffledPack);
                 return true;
             }
@@ -124,6 +117,110 @@ namespace CMP1903M_A01_2223
             }
             return new List<Card>();
         }
-    }
+        public static bool shuffleCardPackTest(int typeOfShuffle)
+        {
+            if (typeOfShuffle == 1)
+            {
+                Random r = new Random();
+                Pack pack = new Pack();
+                List<Card> shuffledPack = new List<Card>();
+                for (int New = pack.pack.Count(); New > 0; New--)
+                {
+                    int Old = r.Next(New);
+                    var temp = pack.pack[Old];
+                    shuffledPack.Add(temp);
+                    pack.pack.RemoveAt(Old);
 
+                }
+                pack.pack = shuffledPack;
+                Console.WriteLine("Pack was shuffled:");
+                foreach (Card card in pack.pack)
+                {
+                    Console.WriteLine(card.Display());
+                }
+                dealTest(shuffledPack);
+                return true;
+            }
+            else if (typeOfShuffle == 2)
+            {
+                // Perform a perfect riffle shuffle
+                Pack pack = new Pack();
+                int half = pack.pack.Count / 2;
+                List<Card> topHalf = pack.pack.GetRange(0, half);
+                List<Card> bottomHalf = pack.pack.GetRange(half, half);
+                List<Card> shuffledPack = new List<Card>();
+
+                for (int i = 0; i < half; i++)
+                {
+                    shuffledPack.Add(topHalf[i]);
+                    shuffledPack.Add(bottomHalf[i]);
+                }
+
+
+                // Replace the pack with the shuffledPack
+                pack.pack = shuffledPack;
+                shuffledPack.Reverse();
+
+                Console.WriteLine("Pack was shuffled:");
+                foreach (Card card in pack.pack)
+                {
+                    Console.WriteLine(card.Display());
+                }
+                dealTest(shuffledPack);
+                return true;
+            }
+
+            else if (typeOfShuffle == 3)
+            {
+                Pack pack = new Pack();
+                dealTest(pack.pack);
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static Card dealTest(List<Card> list)
+        {
+            var deal = list.Take(1);
+            foreach (Card card in deal)
+            {
+                Console.WriteLine("You have been Dealt - " + card.Display());
+            }
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Would you like to be dealt more cards? Please specify how many you wish to be dealt (1-52) ");
+                    int amount = Convert.ToInt32(Console.ReadLine());
+                    if (amount < 1 || amount > 52)
+                    {
+                        throw new Exception();
+                    }
+                    else
+                    {
+                        dealCardTest(amount, list);
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input try again.");
+                    continue;
+                }
+                break;
+            }
+            return new Card(1);
+        }
+        public static List<Card> dealCardTest(int amount, List<Card> list)
+        {
+            var deal = list.Take(amount);
+            foreach (Card card in deal)
+            {
+                Console.WriteLine("You have been Dealt - " + card.Display());
+            }
+            return new List<Card>();
+        }
+    }
 }
+
--- CMP1903M A01 2223/Testing.cs
+++ CMP1903M A01 2223/Testing.cs
@@ -3,23 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CMP1903M_A01_2223;
 
 namespace CMP1903M_A01_2223
 {
-    class Program
+    public class Testing
     {
-        static void Main(string[] args)
+        public static void MainTest()
         {
             Pack pack = new Pack();
+            Console.WriteLine("Initial pack:");
+            foreach (Card card in pack.pack)
+            {
+                Console.WriteLine(card.Display());
+            }
 
             // Shuffle the pack           
-                while (true)
-                {
-                    Console.WriteLine("Type in 1 for a Fisher-Yates Shuffle, 2 to Riffle Shuffle, and 3 for No Shuffle. (4 for testing)");
+            while (true)
+            {
+                Console.WriteLine("Type in 1 for a Fisher-Yates Shuffle, 2 to Riffle Shuffle, and 3 for No Shuffle.");
                 try
                 {
                     int shuffle = Convert.ToInt32(Console.ReadLine());
-                    if (shuffle < 1 || shuffle > 4)
+                    if (shuffle < 1 || shuffle > 3)
                     {
                         throw new Exception();
                     }
@@ -28,33 +34,29 @@ namespace CMP1903M_A01_2223
                         Console.WriteLine();
                         if (shuffle == 1)
                         {
-                            Pack.shuffleCardPack(1);
+                            Pack.shuffleCardPackTest(1);
                         }
                         else if (shuffle == 2)
                         {
-                            Pack.shuffleCardPack(2);
+                            Pack.shuffleCardPackTest(2);
                         }
                         else if (shuffle == 3)
                         {
-                            Pack.shuffleCardPack(3);
-                        }
-                        else if (shuffle == 4)
-                        {
-                            Testing.MainTest();
+                            Pack.shuffleCardPackTest(3);
                         }
                     }
                 }
                 catch
-                { 
+                {
                     Console.WriteLine("Invalid input try again.");
                     continue;
                 }
-                break;               
+                break;
             }
-            Console.WriteLine("Press Enter to exit.");
+            Console.WriteLine("Press Enter to exit test.");
             Console.ReadLine();
 
         }
     }
+                
 }
-
